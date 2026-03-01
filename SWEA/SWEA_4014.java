eedfb8b126928875e0c7c5a3eed4874c48547fb4
Jiyoung Kwak <165020718+KJi0@users.noreply.github.com>
2026-03-01 21:15:07 +0900

    260301

--- SWEA/SWEA_4014.java
+++ SWEA/SWEA_4014.java
@@ -12,6 +12,7 @@ import java.util.StringTokenizer;
 public class SWEA_4014 {
 	static int N, X;
 	static int[][] map;
+	static boolean[] used;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,12 +30,57 @@ public class SWEA_4014 {
 				for (int j = 0; j < N; j++)
 					map[i][j] = Integer.parseInt(st.nextToken());
 			}
-			
-			
+
+			int cnt = 0;
+
+			// 행 검사
+			for (int i = 0; i < N; i++) {
+				if (isOK(map[i])) cnt++;
+			}
+
+			// 열 검사
+			for (int i = 0; i < N; i++) {
+				int[] col = new int[N];
+				for (int j = 0; j < N; j++) col[j] = map[j][i];
+
+				if (isOK(col)) cnt++;
+			}
+
+			System.out.println("#" + t + " " + cnt);
 		}
-		
-		/*
-		 * 한 칸마다 돌면서 상하좌우로 k만큼이 자기보다 1만큼 작으면 true 아니면 false break?
-		 */
+	}
+
+	static boolean isOK(int[] arr) {
+		used = new boolean[N];
+
+		for (int i = 0; i < N - 1; i++) {
+			int diff = arr[i + 1] - arr[i];
+
+			if (diff == 0) continue;
+
+			else if (diff == 1) {
+				for (int j = 1; j < X; j++) {
+					int idx = i - j;
+
+					if (idx < 0) return false;
+					if (arr[idx] != arr[i] || used[idx]) return false;
+
+					used[idx] = true;
+				}
+			}
+
+			else if (diff == -1) {
+				for (int j = 1; j <= X; j++) {
+					int idx = i + j;
+
+					if (idx >= N) return false;
+					if (arr[idx] != arr[i + 1] || used[idx]) return false;
+
+					used[idx] = true;
+				}
+			}
+			else return false;
+		}
+		return true;
 	}
 }
