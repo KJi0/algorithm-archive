b6e002c46ee243d47496eb9ce70e9dad44ae3040
Jiyoung Kwak <165020718+KJi0@users.noreply.github.com>
2026-03-03 14:00:24 +0900

    260303

--- BOJ/BOJ_14890.java
+++ BOJ/BOJ_14890.java
@@ -10,84 +10,71 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_14890 {
-    static int N, L;
-    static int[][] map;
+	static int N, L, ans;
+	static int[][] map;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++)
+				map[i][j] = Integer.parseInt(st.nextToken());
+		}
+		
+		ans = 0;
+		
+		// 행
+		for (int i = 0; i < N; i++) if (simul(map[i])) ans++;
+		
+		// 열
+		for (int i = 0; i < N; i++) {
+			int[] col = new int[N];
+			
+			for (int j = 0; j < N; j++) col[j] = map[j][i];
+			if (simul(col)) ans++;
+		}
+		
+		System.out.println(ans);
+	}
+	
+	static boolean simul(int[] arr) {
+		boolean[] used = new boolean[N];
+		
+		for (int i = 0; i < N - 1; i++) {
+			int diff = arr[i + 1] - arr[i];
+			
+			if (Math.abs(diff) > 1) return false;
+			
+			else if (diff == 1) {
+				for (int j = 0; j < L; j++) {
+					int idx = i - j;
+					
+					if (idx < 0) return false;
+					if (arr[idx] != arr[i] || used[idx]) return false;
 
-        N = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
-        map = new int[N][N];
+					used[idx] = true;
+				}
+			}
+			
+			else if (diff == -1) {
+				for (int j = 1; j <= L; j++) {
+					int idx = i + j;
+					
+					if (idx >= N) return false;
+					if (arr[idx] != arr[i + 1] || used[idx]) return false;
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        int path = 0;
-        for (int i = 0; i < N; i++) {
-            if (checkRow(i)) path++;
-            if (checkCol(i)) path++;
-        }
-
-        System.out.println(path);
-    }
-
-    public static boolean checkRow(int r) {
-        boolean[] isEmpty = new boolean[N];
-
-        for (int i = 0; i < N - 1; i++) {
-            int diff = map[r][i] - map[r][i + 1];
-
-            if (diff > 1 || diff < -1) return false;
-            else if (diff == -1) { // 다음이 한 층 높다
-                for (int j = 0; j < L; j++) {
-                    if (i - j < 0) return false;
-                    if (isEmpty[i - j]) return false;
-                    if (map[r][i] != map[r][i - j]) return false;
-                    isEmpty[i - j] = true;
-                }
-            }
-            else if (diff == 1) { // 다음이 한 층 낮다
-                for (int j = 1; j <= L; j++) {
-                    if (i + j >= N) return false;
-                    if (isEmpty[i + j]) return false;
-                    if (map[r][i + 1] != map[r][i + j]) return false;
-                    isEmpty[i + j] = true;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static boolean checkCol(int c) {
-        boolean[] isEmpty = new boolean[N];
-
-        for (int i = 0; i < N - 1; i++) {
-            int diff = map[i][c] - map[i + 1][c];
-
-            if (diff > 1 || diff < -1) return false;
-            else if (diff == -1) { // 다음이 한 층 높다
-                for (int j = 0; j < L; j++) {
-                    if (i - j < 0) return false;
-                    if (isEmpty[i - j]) return false;
-                    if (map[i][c] != map[i - j][c]) return false;
-                    isEmpty[i - j] = true;
-                }
-            }
-            else if (diff == 1) { // 다음이 한 층 낮다
-                for (int j = 1; j <= L; j++) {
-                    if (i + j >= N) return false;
-                    if (isEmpty[i + j]) return false;
-                    if (map[i + 1][c] != map[i + j][c]) return false;
-                    isEmpty[i + j] = true;
-                }
-            }
-        }
-        return true;
-    }
+					used[idx] = true;
+				}
+			}
+		}
+		
+		return true;
+	}
 }
